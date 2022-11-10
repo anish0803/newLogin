6a8e83c96de0449c59c86b52e768c9adeec66506
anish0803 <105308559+anish0803@users.noreply.github.com>
2022-11-10 20:43:04 +0530

    login created

---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <div>
+        <div className="container">
+        <div className="username">
+      <p>username</p>
+      <input type="text" placeholder='enter user name' />
+      </div>
+
+      <div className="password">
+        <p>password</p>
+        <input type="text" placeholder='enter your password' />
+      </div>
+      
+      <button>Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
